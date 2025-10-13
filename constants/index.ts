f3d028cf733bc0c4e1667278b5106d85d687841a
anu <anirudh157290@gmail.com>
2025-10-13 16:40:49 +0530

    add sidebarLinks array with routes and icons for navigation

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: "Home",
+        route: "/",
+        imgURL: "/icons/Home.svg",
+    },
+    {
+        label: "Upcoming",
+        route: "/upcoming",
+        imgURL: "/icons/Upcoming.svg",
+    },
+    {
+        label: "Previous",
+        route: "/previous",
+        imgURL: "/icons/Previous.svg",
+    },
+    {
+        label: "Recordings",
+        route: "/recordings",
+        imgURL: "/icons/Video.svg",
+    },
+    {
+        label: "Personal Room",
+        route: "/personal-room",
+        imgURL: "/icons/add-personal.svg",
+    },
+]
