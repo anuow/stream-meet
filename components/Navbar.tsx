4f28125a775f2255fb6b2afb43480bccd8134002
anu <anirudh157290@gmail.com>
2025-10-15 00:29:41 +0530

    integrate MobileNav and layout enhancements with flex-between

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
@@ -13,10 +14,16 @@ const Navbar = () => {
           height={32}
           className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Goon</p>
-
-        
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Goon
+        </p>  
       </Link>
+
+      <div className="flex-between gap-5">
+        {/* Clerk User Management */}
+
+        <MobileNav />
+      </div>
     </nav>
   )
 };
